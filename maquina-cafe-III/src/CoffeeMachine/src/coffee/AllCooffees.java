9b2c4a9c5f286b7d070facb5c0cabcff74b51894
Rodrigo Visentainer <rodrigo.visentainer@hotmail.com>
2016-05-02 23:43:18 -0300

    Máquina de Café III

---
+++ maquina-cafe-III/src/CoffeeMachine/src/coffee/AllCooffees.java
@@ -0,0 +1,24 @@
+package coffee;
+
+public class AllCooffees {
+
+	public void coffeeBlack(String value) {
+		value = "Café preto";
+	}
+
+	public void coffeeWithSugar(String value) {
+		value = "Café com açucar";
+
+	}
+
+	public void coffeeWithCream(String value) {
+		value = "Café com creme";
+
+	}
+
+	public void coffeeWithCreamAndSugar(String value) {
+		value = "Café com creme e açucar";
+
+	}
+
+}
